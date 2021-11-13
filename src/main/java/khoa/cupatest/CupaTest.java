f69628cfd18436c9f2391a2ee4199a1d6f5bc1eb
BuildTools <unconfigured@null.spigotmc.org>
2021-11-12 17:54:45 -0700

    CreeperMakeCupa fires off twice, does not work currently

--- src/main/java/khoa/cupatest/CupaTest.java
+++ src/main/java/khoa/cupatest/CupaTest.java
@@ -2,6 +2,7 @@ package khoa.cupatest;
 
 import khoa.cupatest.listeners.CreeperDamageListener;
 import khoa.cupatest.listeners.CreeperExplosionListener;
+import khoa.cupatest.listeners.CreeperMakeCupa;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CupaTest extends JavaPlugin {
@@ -12,6 +13,7 @@ public final class CupaTest extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new CreeperDamageListener(), this);
         getServer().getPluginManager().registerEvents(new CreeperExplosionListener(), this);
+        getServer().getPluginManager().registerEvents(new CreeperMakeCupa(), this);
 
     }
 
---
+++ src/main/java/khoa/cupatest/listeners/CreeperMakeCupa.java
@@ -0,0 +1,34 @@
+package khoa.cupatest.listeners;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+public class CreeperMakeCupa implements Listener {
+
+    @EventHandler
+    public void MakeCreeperCupa(PlayerInteractEntityEvent e){
+
+        Player player = e.getPlayer();
+        Mob mob = (Mob) e.getRightClicked();
+        //player.getInventory().getItemInMainHand();
+        //ItemStack itemStack =
+        if (mob.getType() == EntityType.CREEPER && mob.isAware()){
+            mob.setAware(false);
+            player.sendMessage("Creeper false");
+        }
+        else if (mob.getType() == EntityType.CREEPER && !mob.isAware()){
+            mob.setAware(true);
+            player.sendMessage("Creeper true");
+        }
+        //Entity entity = e.getRightClicked();
+
+    }
+
+}
